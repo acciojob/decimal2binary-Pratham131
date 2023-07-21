dd357b79e7b4cac716709d527b3a3adaf0d6f00a
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-21 13:25:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 function decimalToBinary(num) {
   // Write your code here
-	let arr = [];
 
-	while(num > 1){
-	    arr.push(num%2)
+	if (decimal === 0) return '0';
+
+  let binary = '';
+  // while (decimal > 0) {
+  //   const remainder = decimal % 2;
+  //   binary = remainder + binary; // Store the remainder in reverse order
+  //   decimal = Math.floor(decimal / 2);
+  // }
+
+  // return binary;
+	// let arr = [];
+
+	while(num > 0){
+	    let remainder = (num%2)
+		binary = remainder + binary
 	    num = Math.floor(num/2)
 	}
-	arr.sort((a, b) => b-a)
-	if(num === 1){
-	    arr.unshift(num)
-	}
+	// arr.sort((a, b) => b-a)
+	// if(num === 1){
+	//     arr.unshift(num)
+	// }
 	
 
-	let ans = arr[0];
-	for (let i = 1; i < arr.length; i++) {
-		ans = ans*10 + arr[i];
-	}
+	// let ans = arr[0];
+	// for (let i = 1; i < arr.length; i++) {
+	// 	ans = ans*10 + arr[i];
+	// }
 	return ans.toString();
 }
 
