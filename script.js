a13f5f47992a02aacb4c04062e387f292f228559
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-21 13:07:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function decimalToBinary(num) {
   // Write your code here
+	let arr = [];
+
+	while(num > 1){
+	    arr.push(num%2)
+	
+	    num = Math.floor(num/2)
+	}
+	if(num === 1){
+	    arr.push(num)
+	}
+	arr.sort((a, b) => b-a)
+
+	let ans;
+	for (let i = 0; i < arr.length; i++) {
+		ans = ans*10 + arr[i];
+	}
+	return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
